ce6efc2a2f0314456c10c897190abfd662cdba99
vinicius <vinicius.merelis@gmail.com>
2023-09-26 11:15:42 -0300

    first commit

---
+++ src/test/java/com/curso/swplanetsapi/SwPlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.swplanetsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
